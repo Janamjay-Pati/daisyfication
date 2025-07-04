e554719f13a29fdf41df0c3fcff15d18c72a5f2a
Janamjay <janamjay.pati9@gmail.com>
2025-07-04 15:46:22 +0530

    Added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthenticationComponent } from './components/authentication/authentication.component'; // Import your Auth component
 import { authGuard } from './guards/auth.guard'; // Import the authGuard
+import { EditorComponent } from './components/editor/editor.component';
 
 export const routes: Routes = [
   { path: 'auth', component: AuthenticationComponent }, // Add the auth route
@@ -9,6 +10,10 @@ export const routes: Routes = [
     path: '',
     component: DashboardComponent,
     canActivate: [authGuard], // Protect the dashboard with authGuard
+  },
+  {
+    path: 'editor',
+    component: EditorComponent
   },
   { path: '**', redirectTo: 'auth' }, // Redirect all unknown paths to auth
 ];
